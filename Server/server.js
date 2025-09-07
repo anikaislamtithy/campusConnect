40351f04545c8834c771cfe76f78c6b0abf4a51b
unknown <anika.islam2@g.bracu.ac.bd>
2025-09-07 16:52:51 +0600

    server fix

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie", "X-Requested-With"]
